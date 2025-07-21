e6d6c4a1721aa55ff27bc85417ca0f5f2558e934
Vojta Fluger <vojtech.fluger@gmail.com>
2025-07-21 12:59:12 +0200

    Initial Commit

---
+++ DeliHood/DeliHoodApp.swift
@@ -0,0 +1,17 @@
+//
+//  DeliHoodApp.swift
+//  DeliHood
+//
+//  Created by Vojta Fluger on 21.07.2025.
+//
+
+import SwiftUI
+
+@main
+struct DeliHoodApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
