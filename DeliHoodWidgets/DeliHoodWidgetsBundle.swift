5023b711a18495a736315408058cad4420bc6b37
Vojta Fluger <vojtech.fluger@gmail.com>
2025-10-02 21:51:14 +0200

    merge of order

---
+++ DeliHoodWidgets/DeliHoodWidgetsBundle.swift
@@ -0,0 +1,16 @@
+//
+//  DeliHoodWidgetsBundle.swift
+//  DeliHoodWidgets
+//
+//  Created by Vojta Fluger on 17.09.2025.
+//
+
+import WidgetKit
+import SwiftUI
+
+@main
+struct DeliHoodWidgetsBundle: WidgetBundle {
+    var body: some Widget {
+        DeliHoodWidgetsLiveActivity()
+    }
+}
